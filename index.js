33a0e9269ddb6daa9cedcf2f962dd213a0e8aa81
Omared250 <79874354+Omared250@users.noreply.github.com>
2024-05-27 15:10:59 +0200

    creating and testing endpoints create, update, delete, complete, retake - success

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { connectToDatabase, executeQuery } = require('./database/config');
-const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 
 // create express server
@@ -12,23 +11,8 @@ connectToDatabase();
 // Read and Parse fo the body
 app.use(express.json());
 
-// Create a new task
-app.post('/task', async (req, res) => {
-    const { title, description, dateTime, priority, id, isCompleted, completedDate } = req.body;
-    const query = `
-    INSERT INTO tasks (id, title, description, dateTime, priority, isCompleted, completedDate)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-    RETURNING *
-  `;
-    const params = [id, title, description, dateTime, priority, isCompleted, completedDate];
-
-    try {
-        const result = await executeQuery(query, params);
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// Routes
+app.use('/api/tasks', require('./routes/tasks'));
 
 // listen petitions
 app.listen(process.env.PORT, () => {
